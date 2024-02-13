fb4ade7b7c97304fdd1b9f65b7999b738ec6c84b
ZuhraMasoudBuffaloEdu <zuhramas@buffalo.edu>
2024-02-13 10:54:11 -0500

    Updated the = sign error

--- compiler/src/picco/ast_show.c
+++ compiler/src/picco/ast_show.c
@@ -638,7 +638,7 @@ void ast_batch_print_stmt(aststmt tree, int batch_index, int statement_index, in
         if (tree->u.expr->right->flag == PUB || (tree->u.expr->right->type == CASTEXPR && tree->u.expr->right->left->flag == PUB)) {
             (*narray_element_index)++;
             str_printf(leftop, "_picco_batch_tmp_array%d", *narray_element_index);
-            sprintf(op, " %s ", ASS_symbols[tree->u.expr->opid]);
+            sprintf(op, "%s", ASS_symbols[tree->u.expr->opid]);
         } else if (tree->u.expr->right->flag == PRI) {
             if (tree->u.expr->right->type != BOP) {
                 // consider the casting
