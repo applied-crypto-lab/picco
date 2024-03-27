6d08af8bf9c4fdc8fbb9595325780be54a5b42d9
Alessandro Baccarini <baccaan@gmail.com>
2024-03-27 11:33:21 -0400

    macOS compatibility changes: - including types.h for uint - adding SSE2 header implementation

--- common/shared.h
+++ common/shared.h
@@ -1,6 +1,6 @@
 /*
    PICCO: A General Purpose Compiler for Private Distributed Computation
-   ** Copyright (C) from 2023 PICCO Team
+   ** Copyright (C) from 2013 PICCO Team
    ** Department of Computer Science and Engineering, University at Buffalo (SUNY)
 
    PICCO is free software: you can redistribute it and/or modify
@@ -20,16 +20,20 @@
 #ifndef _SHARED_CONSTANTS_H_
 #define _SHARED_CONSTANTS_H_
 
-// this header houses constants that are common for picco, and picco-util 
+// this header houses constants that are common for picco, and picco-util
 
 // Technique constants aren't used in the compute/ directory, since the techniques are handled directly by CMake, which is responsible for defining/setting the variables
+#define SHAMIR_SS 2
 #define REPLICATED_SS 1
-#define SHAMIR_SS 2 
 
 #define SECURITY_PARAMETER 48 // the security parameter kappa
 
-//these constants ARE common with the compute directory. If they are changed here in compiler/, they must be changed in compute/
+// these constants ARE common with the compute directory. If they are changed here in compiler/, they must be changed in compute/
 #define KEYSIZE 16
 #define BASE 10
+
+#ifdef __APPLE__
+#include <sys/types.h>
+#endif
 
 #endif
