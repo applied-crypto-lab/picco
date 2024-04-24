f1e58288c9c0105edfd698f9fa49460fde5b091d
Alessandro Baccarini <anbaccar@buffalo.edu>
2024-04-23 22:07:58 -0400

    added RSS truncation implementation

--- compute/smc-compute/ops/rss/Trunc.hpp
+++ compute/smc-compute/ops/rss/Trunc.hpp
@@ -22,9 +22,83 @@
 #include "../../NodeNetwork.h"
 #include "../../rss/RepSecretShare.hpp"
 
+// trunation of all data by a single M
 template <typename T>
-void doOperation_Trunc(T **result, T **shares1, int K, int M, int size, int threadID, NodeNetwork net, replicatedSecretShare<T> *ss){}
+void doOperation_Trunc(T **result, T **input, int K, int m, int size, int threadID, NodeNetwork nodeNet, replicatedSecretShare<T> *ss) {
+
+    uint numShares = ss->getNumShares();
+    uint ring_size = ss->ring_size;
+
+    T **edaBit_r = new T *[numShares];
+    T **sum = new T *[numShares];
+    T **r_m_prime = new T *[numShares];
+    T **b = new T *[numShares];
+    T **r_km1 = new T *[numShares];
+
+    T *c = new T[size];
+    memset(c, 0, sizeof(T) * size);
+    T *c_prime = new T[size];
+    memset(c_prime, 0, sizeof(T) * size);
+
+    for (size_t i = 0; i < numShares; i++) {
+        edaBit_r[i] = new T[size];
+        r_m_prime[i] = new T[size];
+        r_km1[i] = new T[size];
+        sum[i] = new T[size];
+
+        b[i] = new T[size];
+        memset(b[i], 0, sizeof(T) * size);
+    }
+
+    T *ai = new T[numShares];
+    memset(ai, 0, sizeof(T) * numShares);
+    ss->sparsify_public(ai, 1);
+
+    edaBit_trunc(edaBit_r, r_m_prime, r_km1, size, ring_size, m, nodeNet);
+
+    // computing the sum of input and edabit_r
+    for (size_t i = 0; i < size; i++) {
+        for (size_t s = 0; s < numShares; s++) {
+            sum[s][i] = (input[s][i] + edaBit_r[s][i]);
+        }
+    }
+
+    Open(c, sum, size, threadID, nodeNet, ss);
+
+    for (size_t i = 0; i < size; i++) {
+
+        // (c / 2^m) mod 2^(k-m-1)
+        c_prime[i] = (c[i] >> T(m)) & ss->SHIFT[ring_size - m - 1];
+        for (size_t s = 0; s < numShares; s++) {
+
+            b[s][i] = r_km1[s][i] + ((c[i] * ai[s]) >> T(ring_size - 1)) - 2 * ((c[i]) >> T(ring_size - 1)) * r_km1[s][i];
+
+            r_m_prime[s][i] = r_m_prime[s][i] - (r_km1[s][i] << T(ring_size - 1 - m));
+
+            result[s][i] = (c_prime[i] * ai[s]) - r_m_prime[s][i] + (b[s][i] << T(ring_size - m - 1));
+        }
+    }
+
+    delete[] ai;
+    delete[] c;
+    delete[] c_prime;
+    for (size_t i = 0; i < numShares; i++) {
+        delete[] edaBit_r[i];
+        delete[] sum[i];
+        delete[] b[i];
+        delete[] r_m_prime[i];
+        delete[] r_km1[i];
+    }
+    delete[] edaBit_r;
+    delete[] b;
+    delete[] sum;
+    delete[] r_m_prime;
+    delete[] r_km1;
+}
+
+// trunation of all data by DIFFERENT m's
 template <typename T>
-void doOperation_Trunc(T **result, T **shares1, int K, int *M, int size, int threadID, NodeNetwork net, replicatedSecretShare<T> *ss){}
+void doOperation_Trunc(T **result, T **input, int K, int *m, int size, int threadID, NodeNetwork nodeNet, replicatedSecretShare<T> *ss) {
+}
 
 #endif // _TRUNC_HPP_
