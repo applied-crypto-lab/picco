fe960ce8a61e99451fd526f68dcd89a34c7c711a
Alessandro Baccarini <anbaccar@buffalo.edu>
2024-05-15 14:37:58 -0400

    finished privWrite prototype

--- compute/smc-compute/ops/rss/PrivIndex.hpp
+++ compute/smc-compute/ops/rss/PrivIndex.hpp
@@ -349,7 +349,6 @@ void doOperation_PrivIndex_Read(T **index, T **array, T **result, int m, int siz
 
     static uint numShares = ss->getNumShares();
     uint ring_size = ss->ring_size;
-    uint total_size = m * size;
     uint logm = ceil(log2(m));
     int dtype_offset = (type == 0) ? 1 : 4;
 
@@ -473,7 +472,122 @@ template <typename T>
 void doOperation_PrivIndex_float_arr(T *index, T ****array, T **result, int dim1, int dim2, int type, int threadID, NodeNetwork nodeNet, replicatedSecretShare<T> *ss) {
 }
 template <typename T>
-void doOperation_PrivIndex_Write(T **index, T **array, T **value, int dim, int size, T *out_cond, T **priv_cond, int counter, int threadID, int type, NodeNetwork nodeNet, replicatedSecretShare<T> *ss) {
+void doOperation_PrivIndex_Write(T **index, T **array, T **value, int m, int size, T *out_cond, T **priv_cond, int counter, int threadID, int type, NodeNetwork nodeNet, replicatedSecretShare<T> *ss) {
+
+    static uint numShares = ss->getNumShares();
+    uint ring_size = ss->ring_size;
+    uint logm = ceil(log2(m));
+    int dtype_offset = (type == 0) ? 1 : 4;
+
+    vector<T> ai(numShares, 0);
+    ss->sparsify_public(ai, 1);
+
+    assertm((ring_size >= logm), "the ring size must be greater than logm");
+
+    T **edaBit_r = new T *[numShares];
+    T **edaBit_b_2 = new T *[numShares];
+    T **sum = new T *[numShares];
+
+    T *c = new T[size];
+
+    for (size_t i = 0; i < numShares; i++) {
+        sum[i] = new T[size];
+
+        edaBit_r[i] = new T[size];
+        edaBit_b_2[i] = new T[size];
+    }
+
+    edaBit(edaBit_r, edaBit_b_2, logm, size, ring_size, nodeNet, ss);
+
+    uint pow_logm = (1 << logm);
+    uint num_bits = (1 << logm) * size; // exact number of bits in the output
+    T **ao_res = new T *[numShares];
+    for (size_t i = 0; i < numShares; i++)
+        ao_res[i] = new T[num_bits];
+
+    AllOr(edaBit_b_2, logm, ao_res, size, -1, nodeNet, ss);
+
+    // computing the complement of the output of AllOr
+    for (size_t s = 0; s < numShares; s++) {
+        for (size_t i = 0; i < num_bits; i++) {
+            ao_res[s][i] = (T(1) & ai[s]) ^ ao_res[s][i];
+        }
+    }
+
+    // converting the complemented output of AllOr to Z2k
+    Rss_B2A(ao_res, ao_res, num_bits, ring_size, nodeNet, ss);
+
+    // [index] + [r]
+    for (size_t s = 0; s < numShares; s++) {
+        for (size_t i = 0; i < size; i++) {
+            sum[s][i] = (index[s][i] + edaBit_r[s][i]);
+        }
+    }
+
+    Open(c, sum, size, -1, nodeNet, ss);
+
+    // obtatining the lower (log m) bits  c mod 2^{log m}
+    for (size_t i = 0; i < size; i++) {
+        c[i] = c[i] & ss->SHIFT[logm];
+    }
+
+    T **A_buff = new T *[numShares];
+    T **B_buff = new T *[numShares];
+    T **C_buff = new T *[numShares];
+    for (size_t s = 0; s < numShares; s++) {
+        A_buff[s] = new T[m * dtype_offset * size];
+        B_buff[s] = new T[m * dtype_offset * size];
+        C_buff[s] = new T[m * dtype_offset * size];
+    }
+
+    int nb;
+    int idx = 0;
+    for (int i = 0; i < size; i++) {
+        idx = 0;
+        for (size_t s = 0; s < numShares; s++) {
+            // A_buff will contain (size) copies of array
+            // memcpy(A_buff[s] + i * dtype_offset * m, array[s], sizeof(T) * dtype_offset * m);
+
+            for (int j = 0; j < m; j++) {
+
+                nb = (c[i] - j) % pow_logm;
+                // printf("nb %d C1 %d j %d pow_logm %d \n", nb, C1, j, pow_logm);
+                // B_buff[s][i * m + nb] = ao_res[s][i * m + nb];
+
+                // not supporting floating point (for now)
+                for (int k = 0; k < dtype_offset; k++) {
+                    A_buff[s][idx] = array[s][j * dtype_offset + k] - value[s][i * dtype_offset + k];
+                    B_buff[s][idx] = ao_res[s][i * m + nb];
+                    idx += 1;
+                }
+            }
+        }
+        // updating array
+        Mult(array, A_buff, B_buff, m * dtype_offset, nodeNet, ss);
+    }
+
+    delete[] c;
+
+    for (size_t i = 0; i < numShares; i++) {
+
+        delete[] A_buff[i];
+        delete[] B_buff[i];
+        delete[] C_buff[i];
+
+        delete[] ao_res[i];
+        delete[] edaBit_r[i];
+        delete[] edaBit_b_2[i];
+        delete[] sum[i];
+    }
+
+    delete[] A_buff;
+    delete[] B_buff;
+    delete[] C_buff;
+
+    delete[] edaBit_r;
+    delete[] edaBit_b_2;
+    delete[] sum;
+    delete[] ao_res;
 }
 template <typename T>
 void doOperation_PrivIndex_Write_2d(T **index, T ***array, int *values, int dim1, int dim2, int size, T *out_cond, T **priv_cond, int counter, int threadID, int type, NodeNetwork nodeNet, replicatedSecretShare<T> *ss) {
