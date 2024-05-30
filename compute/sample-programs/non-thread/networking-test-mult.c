d52292195e5eb544fb05bc8f71f7812768e6d850
mmhartma <mmhartma@buffalo.edu>
2024-05-30 15:27:59 -0400

    thought i committed wrong files, putting back on branch. just the tests

---
+++ compute/sample-programs/non-thread/networking-test-mult.c
@@ -0,0 +1,15 @@
+public int M = 100; 
+
+public int main()
+{
+	private int a, b; 
+    int c[M], d[M];
+	smcinput(a, 1); 
+
+	b = a * a;
+    b = a * b;
+    c = d * d;
+    
+	smcoutput(b, 1);
+	return 0; 
+}
