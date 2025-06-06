c76cd2542d4468ebf42eb750fa35aea3ea00cfbe
erfanhabibipanah <erfanhabibipanah@gmail.com>
2025-06-06 01:00:58 -0400

    Add Shamir_testing file #3

--- compiler/test_fllt_array.c
+++ compiler/test_fllt_array.c
@@ -1,19 +1,16 @@
 #define N 14
 
 int main() {
-    privfloat a[N], b[N];
-    privint cmp_result[N];
+    private float a[N], b[N];
+    private int cmp_result[N];
 
-    // Read N values for each array from party 1
     smcinput(a, 1, N);
     smcinput(b, 1, N);
 
-    // Compare a[i] < b[i] for all i
     for (int i = 0; i < N; ++i) {
         cmp_result[i] = (a[i] < b[i]);
     }
 
-    // Output all results to party 1
     smcoutput(cmp_result, 1, N);
 
     return 0;
