fc16ef1968b20654b6ed08b913553e3e11bf597c
erfanhabibipanah <erfanhabibipanah@gmail.com>
2025-06-06 01:20:16 -0400

    Add test.c file for testing Shamir #3

--- compiler/test.c
+++ compiler/test.c
@@ -1,18 +1,25 @@
 int main() {
     private float a[14], b[14];
     private int cmp_result[14];
+    smcinput(a, 1, 14);
+    smcinput(b, 1, 14);
 
-    smcinput(a, 1, 14);    // Input: party 1 supplies 14 private floats for a
-    smcinput(b, 1, 14);    // Input: party 1 supplies 14 private floats for b
+    cmp_result[0] = a[0] < b[0];
+    cmp_result[1] = a[1] < b[1];
+    cmp_result[2] = a[2] < b[2];
+    cmp_result[3] = a[3] < b[3];
+    cmp_result[4] = a[4] < b[4];
+    cmp_result[5] = a[5] < b[5];
+    cmp_result[6] = a[6] < b[6];
+    cmp_result[7] = a[7] < b[7];
+    cmp_result[8] = a[8] < b[8];
+    cmp_result[9] = a[9] < b[9];
+    cmp_result[10] = a[10] < b[10];
+    cmp_result[11] = a[11] < b[11];
+    cmp_result[12] = a[12] < b[12];
+    cmp_result[13] = a[13] < b[13];
 
-    // Secure element-wise comparison
-    for (int i = 0; i < 14; ++i) {
-        private int tmp;
-        tmp = a[i] < b[i];
-        cmp_result[i] = tmp;
-    }
-    
-    smcoutput(cmp_result, 1, 14); // Output results to party 1
+    smcoutput(cmp_result, 1, 14);
 
     return 0;
 }
