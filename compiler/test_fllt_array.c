ab1544cffa5cc60f5eb635dcb0b859ab280a4501
erfanhabibipanah <erfanhabibipanah@gmail.com>
2025-06-06 01:08:30 -0400

    Add Shamir_testing file #6

--- compiler/test_fllt_array.c
+++ compiler/test_fllt_array.c
@@ -1,14 +1,11 @@
 int main() {
-    private float a[14], b[14];
-    private int cmp_result[14];
+    privint a[2], b[2], cmp_result[2];
+    smcinput(a, 1, 2);
+    smcinput(b, 1, 2);
 
-    smcinput(a, 1, 14);
-    smcinput(b, 1, 14);
+    for (int i = 0; i < 2; ++i)
+        cmp_result[i] = a[i] < b[i];
 
-    for (int i = 0; i < 14; ++i) {
-        cmp_result[i] = priv_lt(a[i], b[i]);
-    }
+    smcoutput(cmp_result, 1, 2);
 
-    smcoutput(cmp_result, 1, 14);
-    return 0;
 }
