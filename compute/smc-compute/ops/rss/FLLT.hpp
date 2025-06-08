a30b6f4fe50acdd9ff660bba0e18e26610ea2755
erfanhabibipanah <erfanhabibipanah@gmail.com>
2025-06-08 19:25:10 -0400

    Add printing for a and b inside FLLT #19

--- compute/smc-compute/ops/rss/FLLT.hpp
+++ compute/smc-compute/ops/rss/FLLT.hpp
@@ -111,7 +111,7 @@ void FLLT(T ***a, T ***b, T **result, uint size, int ring_size, int threadID, No
     for (uint s = 0; s < numShares; s++) {
         for (uint i = 0; i < size; i++) {
             printf("IN BUFFERS\n");
-            printf('a[2][%u][%u] = %f\n', s, i, float(a[2][s][i]));
+            printf('a[2][%u][%u] = %d\n', s, i, a[2][s][i]);
             mult_buffer1[s][i] = a[2][s][i];                // [a.z]
             mult_buffer2[s][i] = b[2][s][i];                    // [b.z]
             mult_buffer1[s][i + size] = a[3][s][i];             // [a.s]
